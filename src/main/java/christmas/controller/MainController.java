bb22cf69d7fb76aee254fefd07f6393020967cdc
oxdjww <oxdjww@gmail.com>
2023-11-12 23:54:02 +0900

    [feat] : `3.1` 주문 메뉴 입력 안내문 출력 기능 구현 완료

--- src/main/java/christmas/controller/MainController.java
+++ src/main/java/christmas/controller/MainController.java
@@ -2,6 +2,7 @@ package christmas.controller;
 
 import static christmas.view.InputView.getDate;
 import static christmas.view.OutputView.printMessage;
+import static christmas.view.constants.ViewConstants.GET_ORDER_MESSAGE;
 import static christmas.view.constants.ViewConstants.WELCOME_MESSAGE;
 
 import christmas.domain.Date;
@@ -9,6 +10,9 @@ import christmas.domain.Date;
 public class MainController {
     void run() {
         printMessage(WELCOME_MESSAGE);
+
         Date date = Date.of(getDate());
+
+        printMessage(GET_ORDER_MESSAGE);
     }
 }
