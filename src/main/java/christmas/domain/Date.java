bfec3f6cfcf8671c1bea703b2409cbc032efb84c
oxdjww <oxdjww@gmail.com>
2023-11-12 22:32:42 +0900

    [feat] : `2.2.1`  입력 날짜가 정수가 아닌 경우 예외를 발생 기능 구현 완료

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,13 +1,30 @@
 package christmas.domain;
 
+import static christmas.exception.constants.ErrorMessage.INVALID_DATE_MESSAGE;
+
+import christmas.exception.EventPlannerException;
+
 public class Date {
     private final int date;
 
     private Date(final String date) {
+        validate(date);
         this.date = Integer.parseInt(date);
     }
 
     public static Date of(final String date) {
         return new Date(date);
+    }
+
+    private void validate(String date) {
+        validateNumeric(date);
+    }
+
+    private void validateNumeric(final String date) {
+        try {
+            Integer.parseInt(date);
+        } catch (NumberFormatException e) {
+            throw EventPlannerException.of(INVALID_DATE_MESSAGE);
+        }
     }
 }
