a063955d720e39a737075ae7985ab08aa9043a37
oxdjww <oxdjww@gmail.com>
2023-11-14 01:56:25 +0900

    [test] : Order 생성 예외 테스트 구현 완료

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -4,20 +4,32 @@ import static christmas.exception.constants.ErrorMessage.INVALID_ORDER;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.exception.EventPlannerException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OrderTest {
 
     @Test
-    @DisplayName("주문 양식이 올바르지 않으면 예외가 발생한다.")
-    void createInvalidFormatOrder() {
+    @DisplayName("주문 수량을 입력하지 않으면 예외가 발생한다.")
+    void createOrderWithNoCount() {
         assertInvalidOrder("타파스-1,제로콜라");
-        assertInvalidOrder("----");
     }
 
     @Test
-    @DisplayName("메뉴의 개수가 1개 이상이 아니면 예외가 발생한다.")
+    @DisplayName("주문 메뉴를 입력하지 않으면 예외가 발생한다.")
+    void createOrderWithNoMenu() {
+        assertInvalidOrder("타파스-1,-3");
+    }
+
+    @Test
+    @DisplayName("주문을 잘못된 양식으로 입력하면 예외가 발생한다.")
+    void createInvalidFormatOrder() {
+        assertInvalidOrder("b");
+    }
+
+    @Test
+    @DisplayName("메뉴의 개수가 1개 이상 정수가 아니면 예외가 발생한다.")
     void createInvalidOrderCount() {
         assertInvalidOrder("타파스-1,제로콜라-0");
         assertInvalidOrder("타파스-&,제로콜라-1");
